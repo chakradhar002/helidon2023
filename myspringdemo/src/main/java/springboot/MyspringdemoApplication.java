b647a70995d0f3ccbc4a2dc4554b199de15f82c0
unknown <mantha747@gmail.com>
2025-02-16 15:08:57 +0530

    hi

---
+++ myspringdemo/src/main/java/springboot/MyspringdemoApplication.java
@@ -0,0 +1,13 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringdemoApplication.class, args);
+	}
+
+}
